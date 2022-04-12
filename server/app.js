8d38d226006c5ad1e404f75d4b4cb509d263a2f5
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-04-12 15:19:58 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const  port = process.env.PORT || 8000; 
+
+const io = require("socket.io")(port, {
+  cors: {
+    origin: "*",
+  },
+});
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log("New User:", name);
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
+});
