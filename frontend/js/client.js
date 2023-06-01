510348ebf925cb8d84298d303249d67410eb5b68
amanswarnakar <amanswarnakar2002@gmail.com>
2023-06-01 10:25:57 +0530

    Update API URI

--- frontend/js/client.js
+++ frontend/js/client.js
@@ -1,4 +1,4 @@
-const socket = io("https://chatapp-backend.up.railway.app");
+const socket = io("https://chatapp-z4wc.onrender.com");
 
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
